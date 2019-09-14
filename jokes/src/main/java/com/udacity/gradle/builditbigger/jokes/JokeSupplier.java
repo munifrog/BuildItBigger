fddecef889df7b4a7586a55af048e4be3fc30ff0
Karl Arthur <karl.crispen.arthur@gmail.com>
2019-09-13 22:22:08 -0600

    feat: Create and use java library to supply jokes

---
+++ jokes/src/main/java/com/udacity/gradle/builditbigger/jokes/JokeSupplier.java
@@ -0,0 +1,7 @@
+package com.udacity.gradle.builditbigger.jokes;
+
+public class JokeSupplier {
+    public static String getJoke() {
+        return "This is supposed to be a funny joke!";
+    }
+}
