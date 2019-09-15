eae2243878171c19e423fc7dac4468cce9d82e53
Karl Arthur <karl.crispen.arthur@gmail.com>
2019-09-15 16:45:23 -0600

    feat: Provide interstitial ads between jokes

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,8 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 import com.udacity.gradle.builditbigger.jokedisplaylibrary.MainJokeActivity;
 
 import static com.udacity.gradle.builditbigger.jokedisplaylibrary.MainJokeActivity.JOKE_DELIVERY_INITIAL;
@@ -18,16 +22,42 @@ import static com.udacity.gradle.builditbigger.jokedisplaylibrary.MainJokeActivi
 public class MainActivity extends AppCompatActivity implements RetrieveJoke.Listener {
     private static int mJokeCount = -1;
     private ProgressBar mProgressBar;
+    private InterstitialAd mInterstitialAd;
+    private Button mTellJoke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTellJoke = findViewById(R.id.btn_tell_joke);
+        mTellJoke.setEnabled(false);
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+                mTellJoke.setEnabled(true);
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                super.onAdFailedToLoad(errorCode);
+                mTellJoke.setEnabled(true);
+            }
+        });
+
         mProgressBar = findViewById(R.id.progress_bar);
         mProgressBar.setVisibility(View.GONE);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,6 +92,14 @@ public class MainActivity extends AppCompatActivity implements RetrieveJoke.List
     @Override
     public void onRetrieved(String intro, String punchline) {
         mProgressBar.setVisibility(View.GONE);
+
+        if (mInterstitialAd.isLoaded()){
+            mInterstitialAd.show();
+        }
+        launchJokeTelling(intro, punchline);
+    }
+
+    private void launchJokeTelling(String intro, String punchline) {
         Intent intent = new Intent(this, MainJokeActivity.class);
         intent.putExtra(JOKE_DELIVERY_INITIAL, intro);
         if (!punchline.isEmpty()) {
